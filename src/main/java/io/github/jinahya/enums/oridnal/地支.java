1764837f6d485d7f66283cecec508dfb2b845691
Jin Kwon <onacit@gmail.com>
2025-08-24 23:25:04 +0900

    wip

---
+++ src/main/java/io/github/jinahya/enums/oridnal/地支.java
@@ -0,0 +1,34 @@
+package io.github.jinahya.enums.oridnal;
+
+/**
+ * Constants of <a href="https://en.wikipedia.org/wiki/Earthly_Branches">the twelve Earthly Branches</a>.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ * @see <a href="https://zh.wikipedia.org/wiki/%E5%9C%B0%E6%94%AF">地支 (Wikipedia)</a>
+ */
+public enum 地支 {
+
+    子, // 자
+
+    丑, // 축
+
+    寅, // 인
+
+    卯, // 묘
+
+    辰, // 진
+
+    巳, // 사
+
+    午, // 오
+
+    未, // 미
+
+    申, // 신
+
+    酉, // 유
+
+    戌, // 술
+
+    亥; // 해
+}
--- src/main/java/io/github/jinahya/enums/oridnal/天干.java
+++ src/main/java/io/github/jinahya/enums/oridnal/天干.java
@@ -1,4 +1,4 @@
-package io.github.jinahya.enums.time;
+package io.github.jinahya.enums.oridnal;
 
 /**
  * Constants of <a href="https://en.wikipedia.org/wiki/Heavenly_Stems">the ten Heavenly Stems</a>.
@@ -8,53 +8,23 @@ package io.github.jinahya.enums.time;
  */
 public enum 天干 {
 
-    /**
-     * The 1st.
-     */
     甲, // 갑
 
-    /**
-     * The 2nd.
-     */
     乙, // 을
 
-    /**
-     * The 3rd.
-     */
     丙, // 병
 
-    /**
-     * The 4th.
-     */
     丁, // 정
 
-    /**
-     * The 5th.
-     */
     戊, // 무
 
-    /**
-     * The 6th.
-     */
     己, // 기
 
-    /**
-     * The 7th.
-     */
     庚, // 경
 
-    /**
-     * The 8th.
-     */
     辛, // 신
 
-    /**
-     * The 9th.
-     */
     壬, // 임
 
-    /**
-     * The 10th.
-     */
     癸; // 계
 }
